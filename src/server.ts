15d613affbe657a28efc52422b760e922fb9709f
kassiokennedy <kassio1206@hotmail.com>
2022-01-18 22:24:07 -0300

    18/01/22-22:24

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@
 import "reflect-metadata"
 import express, { response } from "express"
 
+import "./database"
+
 const app = express();
 
 // http://localhost:3000
-app.listen(3000,()=>console.log("Server is running!"))
+app.listen(3000,()=>console.log("Server is running!!!"))
